c5729a1362eda22cce524a7b54dcb0da9304b1f9
Jeff Clark <116308210+JeffClark1717@users.noreply.github.com>
2023-09-25 15:34:29 -0700

    4.5

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -19,6 +20,41 @@ function App() {
           Learn React
         </a>
       </header>
+=======
+import CitySearch from './components/CitySearch';
+import EventList from './components/EventList';
+import NumberOfEvents from './components/NumberOfEvents';
+import { useEffect, useState } from 'react';
+import { extractLocations, getEvents } from './api';
+
+import './App.css';
+
+const App = () => {
+  const [allLocations, setAllLocations] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [events, setEvents] = useState([]);
+  const [currentCity, setCurrentCity] = useState("See all cities");
+
+
+  useEffect(() => {
+    fetchData();
+  }, [currentCity]);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    const filteredEvents = currentCity === "See all cities" ?
+      allEvents :
+      allEvents.filter(event => event.location === currentCity)
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  }
+
+  return (
+    <div className="App">
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+      <NumberOfEvents />
+      <EventList events={events} />
+>>>>>>> Stashed changes
     </div>
   );
 }
