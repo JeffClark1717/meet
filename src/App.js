0fd4ec4af4dccb8780f3c711905c7608dea6ea42
Jeff Clark <116308210+JeffClark1717@users.noreply.github.com>
2023-10-04 11:05:42 -0700

    correct most errors cause by github branch confusion

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
+import { useEffect, useState } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
-import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
+
 import './App.css';
 
 const App = () => {
-  const [allLocations, setAllLocations] = useState([]);
-  const [currentNOE, setCurrentNOE] = useState(32);
   const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
-
-  const fetchData = async () => {
-    const allEvents = await getEvents();
-    const filteredEvents = currentCity === "See all cities" ?
-      allEvents :
-      allEvents.filter(event => event.location === currentCity);
-    setEvents(filteredEvents.slice(0, currentNOE));
-    setAllLocations(extractLocations(allEvents));
-  }
 
   useEffect(() => {
     fetchData();
-  }, [currentCity]);
+  }, [currentCity, currentNOE]); // Add currentNOE as a dependency
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    const filteredEvents = currentCity === "See all cities"
+      ? allEvents
+      : allEvents.filter(event => event.location === currentCity);
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  };
+
+  const handleInputChange = (event) => {
+    const value = parseInt(event.target.value);
+    setCurrentNOE(value);
+    fetchData();
+  };
 
   return (
     <div className="App">
       <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
-      <NumberOfEvents />
-      <EventList events={events} />
+      <EventList events={events} numberOfEvents={currentNOE} />
+      <NumberOfEvents
+        id="number-of-events-component"
+        numberOfEvents={currentNOE}
+        onInputChange={handleInputChange}// Pass the callback function to the NumberOfEvents component
+      />
     </div>
   );
 }
