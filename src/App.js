09ee0efaecfd8f0a10fb45519c0c7f65e8215108
Jeff Clark <116308210+JeffClark1717@users.noreply.github.com>
2023-10-07 11:38:14 -0700

    4.7

--- src/App.js
+++ src/App.js
@@ -7,52 +7,54 @@ import { extractLocations, getEvents } from "./api";
 import "./App.css";
 
 const App = () => {
-  const [events, setEvents] = useState([]);
-  const [filteredEvents, setFilteredEvents] = useState([]);
-  const [currentNOE, setCurrentNOE] = useState(32);
-  const [allLocations, setAllLocations] = useState([]);
-  const [currentCity, setCurrentCity] = useState("See all cities");
+    const [events, setEvents] = useState([]);
+    const [filteredEvents, setFilteredEvents] = useState([]);
+    const [currentNOE, setCurrentNOE] = useState(32);
+    const [allLocations, setAllLocations] = useState([]);
+    const [currentCity, setCurrentCity] = useState("See all cities");
 
-  const handleCityChange = (city, numberOfEvents) => {
-    setCurrentCity(city);
-    const filteredEvents = events.filter((event) => event.location === city);
-    let sliced = [];
-    if (city === "See all cities") {
-      sliced = events.slice(0, numberOfEvents);
-    } else {
-      //use eventNumber if present to limit number
-      sliced = filteredEvents.slice(0, numberOfEvents);
+    const handleCityChange = (city, numberOfEvents) => {
+        setCurrentCity(city);
+        const filteredEvents = events.filter((event) => event.location === city);
+        let sliced = [];
+        if (city === "See all cities") {
+            sliced = events.slice(0, numberOfEvents);
+        } else {
+            sliced = filteredEvents.slice(0, numberOfEvents);
+        }
+        setFilteredEvents(sliced);
+    };
+
+    function onEventNumberChange(number) {
+        setCurrentNOE(number);
+        handleCityChange(currentCity, number);
     }
-    setFilteredEvents(sliced);
-  };
 
-  function onEventNumberChange(number) {
-    setCurrentNOE(number);
-    handleCityChange(currentCity, number);
-  }
+    useEffect(() => {
+        async function fetchData() {
+            const allEvents = await getEvents();
+            setAllLocations(extractLocations(allEvents));
+            setEvents(allEvents);
+        }
+        fetchData();
+    }, []);
 
-  useEffect(() => {
-    async function fetchData() {
-      const allEvents = await getEvents();
-      setAllLocations(extractLocations(allEvents));
-      setEvents(allEvents);
-    }
-    fetchData();
-  }, []);
+    return (
+        <div className="App">
+            <h1>Meet App</h1>
+            <CitySearch
+                allLocations={allLocations}
+                setCurrentCity={handleCityChange}
+            />
+            <NumberOfEvents
+                currentNOE={currentNOE}
+                setCurrentNOE={onEventNumberChange}
+            />
+          <div className="events-container"></div>
+            <EventList events={filteredEvents.length > 0 ? filteredEvents : events} />
 
-  return (
-    <div className="App">
-      <CitySearch
-        allLocations={allLocations}
-        setCurrentCity={handleCityChange}
-      />
-      <NumberOfEvents
-        currentNOE={currentNOE}
-        setCurrentNOE={onEventNumberChange}
-      />
-      <EventList events={filteredEvents.length > 0 ? filteredEvents : events} />
-    </div>
-  );
+        </div>
+    );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+
+atatus.config('30d92e78563741d79c43d0531ce3b707').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -21,3 +25,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
